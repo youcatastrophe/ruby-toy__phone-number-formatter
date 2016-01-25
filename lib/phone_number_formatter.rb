bc44706c95038f26475aef4e6038b5e8396c6fb2
Ryan Newell <youcatastrophe@gmail.com>
2016-01-25 14:49:51 -0600

    That was... fun

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,12 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  
+    just_numbers = phone_number_str.scan(/\d/)
+    just_numbers_str = just_numbers.join("")
+    area_code = just_numbers_str[0, 3]
+    prefix = just_numbers_str[3, 3]
+    last = just_numbers_str[6, 4]
+    final_string = "(" + area_code + ")" + " " + prefix + "-" + last
+    final_string
 end
+puts format_phone_number("402-699. 2450")
